bedf91e21b1ec6375c4f164f0e47fe4b2727648b
ThalitaSaniyyaAqillaPutri <79181196+ThalitaSaniyyaAqillaPutri@users.noreply.github.com>
2022-03-06 18:23:51 +0700

    Membuat StateFullComponent

--- Praktikum_3/web-reactku/src/index.js
+++ Praktikum_3/web-reactku/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function HelloComponent(){
+function HelloComponent () {
   return HelloComponent
-ReactDOM.render(<HelloComponent />, document.getElementById('root'));  
+// ReactDOM.render(<HelloComponent />, document.getElementById('root'));  
 }
 
+class StateFullComponent extends React.Component {
+  render() {
+    return<p>StateFullComponent</p>
+  }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StateFullComponent />    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
