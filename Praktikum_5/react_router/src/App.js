714e7a2c4ed73faee3fc6b5875f4226d0d210421
ThalitaSaniyyaAqillaPutri <79181196+ThalitaSaniyyaAqillaPutri@users.noreply.github.com>
2022-03-21 22:41:39 +0700

    Praktikum 4

--- Praktikum_5/react_router/src/App.js
+++ Praktikum_5/react_router/src/App.js
@@ -1,83 +1,119 @@
 import React from "react";
-import {
+import  {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  useParams,
-  useRouteMatch
-} from 'react-router-dom';
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 
-export default function NestingExample() {
-  return (
-      <Router>
-          <div>              
-              <ul>
-                  <li>
-                      <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                      <Link to="/topics">Topics</Link>
-                  </li>                 
-              </ul>
-              <hr />
-
-              <Switch>
-                  <Route exact path="/">
-                    <Home/>
-                  </Route>
-                  <Route path="/topics">
-                    <Topics/>
-                  </Route>
-              </Switch>
-          </div>
-      </Router>
-  );
-}
-
-function Home() {
-  
-  return (
-      <div>
-          <h2>Home</h2>
-      </div>
-  );
-}
-
-function Topics(){
-  let {path, url} = useRouteMatch();
+export default function AuthExample() {
   return(
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${url}/Sate, Nasi goreng`}>Kuliner</Link>
-        </li>
-        <li>
-          <Link to={`${url}/Wisata alam, Museum`}>Travelling</Link>
-        </li>
-        <li>
-          <Link to={`${url}/Ibis, JW Marriot`}>Review Hotel</Link>
-        </li>
-      </ul>
+    <Router>
+      <div>
+        <Switch>
+          <AuthButton />
+        </Switch>
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/private">Private Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/public">
+            <PublicPage />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute path="/private">
+            <ProtectedPage />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 
-      <Switch>
-        <Route exact path={path}>
-            <h3>Please select a topic.</h3>
-        </Route>
-        <Route path={`${path}/:topicId`}>
-            <Topic />
-        </Route>
-      </Switch>
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function AuthButton() {
+  let history = useHistory();
+
+  return fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome!{" "}
+      <button 
+        onClick={() => {
+          fakeAuth.signout(() => history.push("/"));
+        }}>
+        Sign out
+      </button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  );
+}
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: location }
+          }}
+          />
+        )
+      
+      }
+    ></Route>
+  );
+}
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function ProtectedPage() {
+  return <h3>Private</h3>
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/"} };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You mush log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
     </div>
   );
 }
-  function Topic(){
-    let {topicId} = useParams();
-
-    return(
-      <div>
-        <h3>{topicId}</h3>
-      </div>
-    );
-  }
