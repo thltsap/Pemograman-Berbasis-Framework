b8ef5bedc34a607de874d4113bd4d0f3f1e58641
ThalitaSaniyyaAqillaPutri <79181196+ThalitaSaniyyaAqillaPutri@users.noreply.github.com>
2022-04-13 23:09:51 +0700

    Praktikum 2

--- Praktikum_4/web-reactku/src/Services/index.js
+++ Praktikum_4/web-reactku/src/Services/index.js
@@ -11,11 +11,33 @@ const GetAPI = (path) => {
     })
     return promise;
 }
+const PostAPI = (path, data) =>{
+    const promise = new Promise((resolve, reject) => {
+        fetch(`${domainPath}/${path}`,{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+         .then((result) => {
+             resolve(result);
+         }, (err) => {
+             reject(err);
+         })
+    })
+    return promise;
+}
+
+
 
 const getNewsBlog = () => GetAPI('posts?_sort=id&_order=desc');
+const postNewsBlog = (dataYgDikirim) => PostAPI('posts', dataYgDikirim);
 
 const API ={
     getNewsBlog,
+    postNewsBlog
     
 }
 export default API;
--- Praktikum_4/web-reactku/src/container/BlogPost.jsx
+++ Praktikum_4/web-reactku/src/container/BlogPost.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import './BlogPost.css';
-import Post from '../component/BlogPost/Post';
-import API from '../Services/index.js';
+import Post from "../component/BlogPost/Post"
+import API from "../Services/index"
 
 class BlogPost extends Component{
     state = {
@@ -22,22 +22,22 @@ class BlogPost extends Component{
             })
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.ambilDataDariServerAPI()
     }
 
     handleHapusArtikel = (data) => {
         fetch(`http://localhost:3001/posts/${data}`, { method: 'DELETE' })
-            .then(() => {
+            .then(res => {
                 this.ambilDataDariServerAPI()
             })
     }
 
     handleTambahArtikel = (event) => {
-        let formInsertArtikel = { ...this.state.insertArtikel }
-        let timestamp = new Date().getTime()
-        formInsertArtikel['id'] = timestamp
-        formInsertArtikel[event.target.name] = event.target.value
+        let formInsertArtikel = { ...this.state.insertArtikel };
+        let timestamp = new Date().getTime();
+        formInsertArtikel['id'] = timestamp;
+        formInsertArtikel[event.target.name] = event.target.value;
         this.setState({
             insertArtikel: formInsertArtikel
         })
@@ -50,35 +50,35 @@ class BlogPost extends Component{
             });
     }
 
-
     render() {
-        return (            
-                <div className="post-artikel">
-                    <div className="form pb-2 border-bottom">
-                        <div className="form-group-row">
-                            <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
-                            <div className="col-sm-10">
-                                <input type="text" className="form-control" name="title" id="title" onChange={this.handleTambahArtikel} />
-                            </div>
+        return (
+            <div className="post-artikel">
+                <div className="form pb-2 border-bottom">
+                    <div className="form-group-row">
+                        <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel} />
                         </div>
-                        <div className="form-group-row">
-                            <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
-                            <div className="col-sm-10">
-                                <textarea className="form-control" id="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
-                            </div>
-                        </div>
-    
-                        <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
                     </div>
-    
-                    <h2>Daftar Artikel</h2>
-                    {
-                        this.state.listArtikel.reverse().map(artikel => {
-                            return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel} />
-                        })
-                    }
+                    <div className="form-group-row">
+                        <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
                 </div>
+
+                <h2>Daftar Artikel</h2>
+                {
+                    this.state.listArtikel.map(artikel => {
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel} />
+                    })
+                }
+            </div>
         )
     }
 }
-export default BlogPost;    
+
+export default BlogPost;
